5173bda8a86f078e805a59d9ee34fb956ef3b261
scroll <ymarumar502@gmail.com>
2025-08-09 02:45:59 -0500

    Обновление конфигурации ASGI: добавлена явная инициализация Django перед импортом модулей, использующих ORM. Упрощена структура импорта и улучшена читаемость кода.

--- backend/vrachiapp_backend/asgi.py
+++ backend/vrachiapp_backend/asgi.py
@@ -8,14 +8,19 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+
+# ВАЖНО: сначала указываем настройки Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vrachiapp_backend.settings')
+
+# Явно инициализируем Django до импорта модулей, использующих ORM/модели
+import django
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 from channels.sessions import SessionMiddlewareStack
-from authentication.routing import websocket_urlpatterns
 from authentication.middleware import WebSocketAuthMiddleware
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vrachiapp_backend.settings')
+from authentication.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
